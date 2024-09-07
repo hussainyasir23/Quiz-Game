993de6d05df8f0199d7df4a667f179d5b27a02a7
Yasir Hussain <hussainyasir23@gmail.com>
2024-09-07 15:18:47 +0530

    Arrange Files & Remove Constants File for now.

---
+++ Models/Enums/QuestionType.swift
@@ -0,0 +1,22 @@
+//
+//  QuestionType.swift
+//  Quiz Game
+//
+//  Created by Yasir on 07/09/24.
+//
+
+import Foundation
+
+enum QuestionType: String, CaseIterable {
+    case any = ""
+    case multiple = "multiple"
+    case boolean = "boolean"
+    
+    var displayName: String {
+        switch self {
+        case .any: return "Any Type"
+        case .multiple: return "Multiple Choice"
+        case .boolean: return "True / False"
+        }
+    }
+}
